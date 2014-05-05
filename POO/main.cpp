83593d0f11d5cd2b8b850786d65c8231532ea3cb
Dan Stativa <dan.stativa@gmail.com>
2014-05-05 21:41:59 +0200

    somehow it breaks

--- POO/main.cpp
+++ POO/main.cpp
@@ -7,5 +7,13 @@ int main(int argc, char** argv) {
 	
 	Arbore<int> arb;
 	
+	arb.afiseaza();
+	
+	int v[] = {2, 23, 3, 142, 14, 57};
+    for(int i = 0; i < 6; i++)
+        arb + (v[i]);
+        
+    arb.afiseaza();
+	
 	return 0;
 }
