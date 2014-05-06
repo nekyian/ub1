76424f7a1f1b56e607c0e3738dae42b9ba06d31a
Dan Stativa <dan.stativa@gmail.com>
2014-05-06 23:24:47 +0300

    cleanup

--- Proiect - DevC++/Arbore.h
+++ Proiect - DevC++/Arbore.h
@@ -1,111 +1,82 @@
-#include<iostream>
-#include <typeinfo>
+#include <iostream>
 
 using namespace std;
 
 template <class T>
-class Node
-{
-	public:
-		T data;
-		Node<T> *left;
-		Node<T> *right;
-		
-		Node<T>(T const&);
-};
-
-template <class T>
-Node<T>::Node(T const& element = NULL)
-{
-	if(element == NULL) {
-		
-	} else {
-		data = element;
-		left = NULL;
-		right = NULL;
-	}
-}
-
-template <class T>
 class Arbore
 {
+
+    typedef struct bst
+    {
+        T data;
+        struct bst *left, *right;
+    }node;
+
+    node *root, *New, *temp, *parent;
+
+    public: Arbore()
+    {
+        root = NULL;
+    }
+    
+    void create();
+    void operator+(T const& element);
+	void insert(node *root, node *New);
 	
-	Node<T> *root, *New, *temp, *parent;
+	void afiseaza();
+	void inorder(node *temp);
+	void postorder(node *temp);
 	
-	public:
-        Arbore(T const&);	// constructor
-        int vida();	// helper method returneaza 1 daca arborele e gol
-        ~Arbore();	// destructor - elimina nod cu nod pana cand arborele e gol (radacina == NULL)
-        
-        void create(T const&);
-        
-        void operator+(T const&);				// operator de inserare cheie in Arbore
-        void insert(Node<T> *, Node<T> *);	// functie de inserare cheie in Arbore
-        
-        void operator-(T const&);				// operator de stergere cheie din Arbore
-        
-        void inorder(Arbore<T> *);		// parcurgere inordine a Arborelui
-        void afiseaza();
-        
-        
-        
 };
 
 template <class T>
-Arbore<T>::Arbore(T const& element = NULL)
+void Arbore<T>::create()
 {
-	if(element == NULL)
-    	root=NULL;
-    	
-    cout << "am creeat arborele\n";
-}
+	New = new node;
+    New->left = NULL;
+    New->right = NULL;
 
-template <class T>
-int Arbore<T>::vida(){return root==NULL;}
+    cout << "\n introduceti primul element al arborelui: \n";
+    cin >> New->data;
 
-template <class T>
-Arbore<T>::~Arbore()
-{
-//    int n;
-//    while(!vida()) {
-//    	root-(n);
-//    	cout << "am scos cheia " << n << "din Arbore" << endl;
-//    }   
-//    cout << "am scos toate cheile din arbore" << endl;
-}
-
-// metoda care creeaza un Arbore
-template <class T>
-void Arbore<T>::create(T const&)
-{
-	
+    if(root == NULL) {
+    	root = New;
+    	cout << "am creeat radacina arborelui, cu valoare " << New->data << endl;
+    }
+    else
+        insert(root, New);
 }
 
 // operator de inserare cheie in Arbore
 template <class T>
 void Arbore<T>::operator+(T const& element)
 {
-	// cream un Arbore nou din elementul primit
-	New = new Node<int>(element);
-		
-	// inseram Arborele nou creat in Arborele principal
-	insert(root, New);
+    // cream un Nod nou din elementul primit
+    New = new bst;
+    New->data = element;
+    New->left = NULL;
+    New->right = NULL;
+    cout << "Am creeat un nod nou:\ndata: " << element << endl;
+        
+    // inseram Nodul nou creat in Arborele principal
+    insert(root, New);
 }
 
-//functie de inserare cheie in Arbore
 template <class T>
-void Arbore<T>::insert(Node<T> *root, Node<T> *New)
+void Arbore<T>::insert(node *root, node *New)
 {
-	
-	// daca valoarea noului element e mai mica decat valoarea radacinii creem un subarbore stang
+    // daca valoarea noului element e mai mica decat valoarea radacinii creem un subarbore stang
     if(New->data < root->data)
     {
         if(root->left == NULL) {
         	root->left = New;
-            cout << "am inserat o frunza stanga" << endl;
+            cout << "am introdus nodul nou ca copil stang" << endl;
         }
-        else
-            insert(root->left, New);
+        else {
+        	cout << "ne deplasam in jos stanga" << endl;
+        	insert(root->left, New);
+        }
+        	
     }
 
     // daca valoarea noului element e mai mare decat valoarea radacinii creem un subarbore drept
@@ -113,45 +84,61 @@ void Arbore<T>::insert(Node<T> *root, Node<T> *New)
     {
         if(root->right == NULL) {
         	root->right = New;
-            cout << "am inserat o frunza dreapta" << endl;
+        	cout << "am introdus nodul nou ca copil drept" << endl;
         }
-        else
-            insert(root->right, New);
+        else {
+        	cout << "ne deplasam in jos dreapta" << endl;
+        	insert(root->right, New);
+        }
+            
     }
-	 
-}
 
-// operator de stergere cheie din Arbore
-template <class T>
-void Arbore<T>::operator-(T const& element)
-{
-	//parcurgem Arborele si localizam cheia
-	
-	//se disting mai multe cazuri in functie de pozitia cheii in Arbore
-	
-}
-
-template <class T>
-void Arbore<T>::inorder(Arbore<T> *temp)
-{
-	if(temp != NULL)
-    {
-        inorder(temp->left);
-        cout << " " << temp->data;
-        inorder(temp->right);
-    }
 }
 
 template <class T>
 void Arbore<T>::afiseaza()
 {
-	cout << "aici";
-	
-	if(root == NULL)
+    if(root == NULL)
         cout << "arborele nu este creat";
     else
     {
-        cout << "\n arborele este: ";
-        inorder(root);
+    	cout << "alegeti tipul de parcurgere dorit:\n1. inordine\n2.postordine\n";
+    	int tip;
+		cin >> tip;
+		cout << "\n arborele este: ";
+		switch(tip)
+		{
+			case 1:
+				inorder(root);
+				break;
+			case 2:
+				postorder(root);
+				break;
+		}
     }
 }
+
+template <class T>
+void Arbore<T>::inorder(node *temp)
+{
+
+    if(temp != NULL)
+    {
+        inorder(temp->left);
+        cout << " " << temp->data;
+        inorder(temp->right);
+    }
+
+}
+
+template <class T>
+void Arbore<T>::postorder(node *temp)
+{
+    if(temp != NULL)
+    {
+        postorder(temp->right);
+        cout << " " << temp->data;
+        postorder(temp->left);
+    }
+}
+
--- Proiect - DevC++/main.cpp
+++ Proiect - DevC++/main.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
+#include <string>
 #include "Arbore.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	
-	Arbore<int> arb;
+//	Arbore<int> arb;
+//	
+//	arb.create();
+//	
+//	int v[] = {2, 23, 3, 142, 14, 57};
+//    for(int i = 0; i < 6; i++)
+//        arb + (v[i]);
+//        
+//    arb.afiseaza();
+    
+    Arbore<string> arb2;
+    
+    arb2.create();
+    int v[] = {"a", "c", "b", "d"};
+    for(int i = 0; i < 4; i++)
+    	arb2 + v[i];
 	
-	int v[] = {2, 23, 3, 142, 14, 57};
-    for(int i = 0; i < 6; i++)
-        arb + (v[i]);
-        
-    arb.afiseaza();
+	arb2.afiseaza();
 	
 	return 0;
 }
