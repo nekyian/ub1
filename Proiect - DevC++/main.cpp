e73ceaccee2b6d8a56ac2fae38fc3c9169e56c70
09c <09c@users.noreply.github.com>
2014-05-16 03:55:28 +0300

    c+arbore;char arbore = c;

--- Proiect - DevC++/main.cpp
+++ Proiect - DevC++/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 //#include <string> http://www.cplusplus.com/reference/string/
 #include "Arbore.h"
+using namespace std;
+
 
 //int main(int argc, char** argv)
 //deocamdata nu pasam argumente cu argc si argv
@@ -10,90 +12,99 @@ int main()
 {
 
 
-    Arbore<char> arb1,arb2,arb3,arb4,arb5,arb6,arb7; // incercam cu char deocamdata fiind tip fundamental
+    Arbore<char> arb1,arb2,arb3,arb4,arb5,arb6,arb7,arb8; // incercam cu char deocamdata fiind tip fundamental
 	char sters,opt;
-
+	
 	char v1[] = {'d', 'e', 'f', 'a','b','c','7'};  // delimitare cu ' pentru vectori de char
     arb1.create(v1[0]); //radacina este primul element
-    for(int i = 1; i < sizeof(v1); i++)
+    for(unsigned i = 1; i < sizeof(v1); i++) 
 		arb1+v1[i];
-
-
-
-
-
+		
+		
+	   
+    
+    
     char v2[]={'a', 'b', 'c', 'w','5'} ;  //arb22 pentru testare operator arbore+arbore
     arb2.create(v2[0]);                             //initializare radacina arb22
-    for(int i = 1; i < sizeof(v2); i++)
+    for(unsigned int i = 1; i < sizeof(v2); i++) 
 		arb2+v2[i];
+		
 
-
-
-    //decomenteaza pt. stergere  Arb2-cheie
+    
+    //decomenteaza pt. stergere  Arb2-cheie	
 //    arb2.afiseaza();
 //    cout << "\n doriti sa stergeti un element? y/n \n";
 //    cin >> opt;
-//    while (opt=='y')
+//    while (opt=='y') 
 //	{
-//
+//		 
 //    	cout << "\n introduceti elementul de sters \n";
 //    	cin >> sters;
 //    	arb2-sters;
 //    	arb2.afiseaza();
 //    	cout << "\n doriti sa stergeti un element? y/n \n";
 //        cin >> opt;
-//
+//    	
 //    	    }
-//
+//  
 //  arb2.afiseaza();
 //
-    cout<<"\n arb1: ";
-    cout<<"\n";
-    arb1.afiseaza();
-    cout<<"\n arb2: ";
-    cout<<"\n";
-    arb2.afiseaza();
-    cout<<"\n arb1+arb2 = ";
-    cout<<"\n";
-    arb3=arb1+arb2;
-    arb3.afiseaza();
+   cout<<"\n arb1: ";
+   cout<<"\n";
+   arb1.afiseaza();
+   cout<<"\n arb2: ";
+   cout<<"\n";
+   arb2.afiseaza();
+   cout<<"\n arb1+arb2 = ";
+   cout<<"\n";
+   arb3=arb1+arb2;
+   arb3.afiseaza();
 
-    cout << "---------------" << endl;
 
-    //testare arb5-arb6
 
-    cout << "testare arb5-arb6" << endl;
-
+ //testare arb5-arb6    
     char v5[] = {'d', 'e', 'f', 'a','b','c','7'};  // delimitare cu ' pentru vectori de char
     arb5.create(v5[0]); //radacina este primul element
-    for(int i = 1; i < sizeof(v5); i++)
+    for(unsigned int i = 1; i < sizeof(v5); i++) 
 		arb5+v5[i];
-
-    char v6[] = {'a', 'b', '7'};  // delimitare cu ' pentru vectori de char
+			
+		char v6[] = {'a', 'b', '7'};  // delimitare cu ' pentru vectori de char
     arb6.create(v6[0]); //radacina este primul element
-    for(int i = 1; i < sizeof(v6); i++)
-
-
-    arb6+v6[i];
-    cout<<"\n arb5: ";
-    cout<<"\n";
-    arb5.afiseaza();
-    cout<<"\n arb6: ";
-    cout<<"\n";
-    arb6.afiseaza();
-
-    cout << "---------------" << endl;
-
-    arb4=arb5-arb6;
-    cout<<"\n arb5-arb6 = ";
-    cout<<"\n";
-    arb4.afiseaza();
-
-    cout << "---------------" << endl;
-
-    //afisare comparatie == 2 arbori
-
-    Arbore<int> arbx, arby;
+    for(unsigned int i = 1; i < sizeof(v6); i++) 
+    
+    
+		arb6+v6[i];  
+cout<<"\n arb5: ";
+cout<<"\n";
+   arb5.afiseaza();
+cout<<"\n arb6: ";
+cout<<"\n";
+   arb6.afiseaza(); 
+    
+   arb4=arb5-arb6; 
+cout<<"\n arb4 = arb5-arb6 = ";  
+cout<<"\n";
+   arb4.afiseaza();
+   
+      
+   char c='K';
+   cout <<"\n "<<c<<" + arb4 = \n";
+   c+arb4;  
+   arb4.afiseaza();
+   
+   
+   arb7 = 'W'; 
+   cout << "\n initializare arb7 = 'W' \n \n arb7 = \n";          
+   arb7.afiseaza(); 
+ 
+ 
+   cout << "\n arb8 = arb4 \n \n arb8 = \n";
+   arb8=arb4; //TODO
+   arb8.afiseaza();
+   
+   
+   
+   Arbore<int> arbx, arby;
     int vx[5] = {1, 2, 3, 4, 5};
     arbx.create(vx[0]);
     arby.create(vx[0]);
@@ -103,17 +114,24 @@ int main()
     }
     arbx + 6;
 
-    cout << "------------------" << endl;
+    cout << "_______________________" << endl;
     cout << "arb1: ";
     arbx.afiseaza();
-    cout << "------------------" << endl;
+    //cout << "------------------" << endl;
     cout << "arb2: ";
     arby.afiseaza();
-    cout << "------------------" << endl;
+    //cout << "------------------" << endl;
 
     cout << (arbx==arby) << endl;
 
+    cout << "_______________________" << endl;
+   
+   
+   
 
+   
+    
+    
     return 0;
 }
 
