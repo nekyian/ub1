7465343ac1b902d854d4d7352f4f4f8da908c72b
09c <09c@users.noreply.github.com>
2014-05-20 03:50:49 +0300

    +/- contor

--- Proiect - DevC++/Nod.h
+++ Proiect - DevC++/Nod.h
@@ -1,9 +1,9 @@
-template <class T>
-class node
-{
-    public:
-        T data;
-        node *right,*left;
-        node(){right = NULL; left = NULL;}
-        node(const T& d){right = NULL; left = NULL; data = d;}
-};
+template <class T> class node  
+		{
+			public:
+				T data;
+				unsigned int contor=1; // o cheie 'X' care apare de 5 ori in arbore se va afisa ca X{5}
+				node *right,*left;
+				node(){right = NULL; left = NULL;}
+				node(const T& d){contor=1;right = NULL; left = NULL; data = d;}
+		};
