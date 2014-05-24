f37eefa16c89565e78b5df7b84afaa8059258265
09c <cosmin_nv@yahoo.com>
2014-05-25 00:06:08 +0300

    cautare simpla

--- Nod.h
+++ Nod.h
@@ -2,7 +2,8 @@ template <class T> class node
 		{
 			public:
 				T data;
-				unsigned int contor=1; // o cheie 'X' care apare de 5 ori in arbore se va afisa ca X{5}
+				unsigned int contor=1;
+				// o cheie 'X' care apare de 5 ori in arbore se va afisa ca X{5}
 				node *right,*left;
 				node(){right = NULL; left = NULL;}
 				node(const T& d){contor=1;right = NULL; left = NULL; data = d;}
